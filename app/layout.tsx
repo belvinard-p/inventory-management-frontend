0bedc5f072be8bbd3d032c87bcdfb424727fb23f
belvip <pouadjeubelvi@gmail.com>
2025-11-23 10:13:56 +0000

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased" suppressHydrationWarning>
         <QueryProvider>
           <ThemeProvider
             attribute="class"
